409e9f2fc9d3fbd0691cea87d86de0e02950d8a9
Leonardo Hernandez <54067414+hernandez00@users.noreply.github.com>
2022-09-22 10:25:34 -0300

    separated-functions-to-fill

--- experimentalTest.py
+++ experimentalTest.py
@@ -7,7 +7,7 @@
 # ID's: cpfCnpj, name, nameMother, rg, rgIssuer, cnh, cnhNumber (Número de segurança),
 # dddPhone, phone, dddOne, phoneOne, email, addressZipCode, addressNumber
 
-# Campos dropdown:    //ejs-dropdownlist[@formcontrolname='gender']
+# Campos dropdown:    //ejs-dropdownlist[@formcontrolname='{locator}']
 # ID's: gender, codCareer, cnhIsoCountry, cnhState, addressCoutry
 
 import json
@@ -23,43 +23,63 @@ from selenium.common.exceptions import ElementClickInterceptedException as ECIex
 driver = webdriver.Chrome(service=ChromeService(
     ChromeDriverManager().install()))
 driver.set_window_size(1600, 900)
-driver.get("https://easy.unidas.com.br/login")
-#driver.get("https://easy.hml.unidas.com.br/login")
+#driver.get("https://easy.unidas.com.br/login")
+driver.get("https://easy.hml.unidas.com.br/login")
 wait = WebDriverWait(driver, 10)
 
-
+# Função para ler o arquivo jSon
 def file_reading(filedir):
     with open(filedir, 'r') as file:
         person = json.loads(file.read())
     return person
 
-def textInteract():
-    pass
+# Função para preencher campos do tipo Text
+def textInteract(locator, value):
+    element = wait.until(EC.visibility_of_element_located((
+        By.XPATH, f"//input[@formcontrolname = '{locator}']")))
 
-def datePickerInteract():
-    pass
+    element = wait.until(EC.presence_of_element_located((
+        By.XPATH, f"//input[@formcontrolname = '{locator}']")))
+    
+    element.send_keys(value)
 
-def comboInteract():
-    pass
+# Função para preencher campos do tipo Date Picker
+def datePickerInteract(locator, value):
+    element = wait.until(EC.visibility_of_element_located((
+        By.XPATH, f"//ejs-datepicker[@formcontrolname='{locator}']//input")))
 
+    element = wait.until(EC.presence_of_element_located((
+        By.XPATH, f"//ejs-datepicker[@formcontrolname='{locator}']//input]")))
+    
+    element.send_keys(value)
 
+# Função para preencher campos do tipo ComboBox
+def comboInteract(locator, value):
+    element = wait.until(EC.visibility_of_element_located((
+        By.XPATH, f"//ejs-dropdownlist[@formcontrolname='{locator}']")))
+
+    element = wait.until(EC.presence_of_element_located((
+        By.XPATH, f"//ejs-dropdownlist[@formcontrolname='{locator}']")))
+    
+    element.send_keys(value)
+
+# Função para percorrer o arquivo jSon e chamar as funções
+# de preenchimento de acordo com o tipo do campo.
 def registration_filling(filedir="./person.json"):
     person = file_reading(filedir)
-    for k_data, v_data in person.items():
+    for client, data in person.items():
+        print(client)
+        for k_data, v_data in data.items():
             print(k_data)
             for k, v in v_data.items():
-                if k_data == "text":
-                    locator = f"//input[@formcontrolname = '{k}']"
-                elif k_data == "datePicker":
-                    locator = f"//ejs-datepicker[@formcontrolname='{k}']//input"
-                else:
-                    locator = f"//ejs-dropdownlist[@formcontrolname='{k}']"
+                print(f"{k}, {v}")
                 try:
-                    element = wait.until(EC.visibility_of_element_located((
-                        By.XPATH, locator)))
-
-                    element = wait.until(EC.presence_of_element_located((
-                        By.XPATH, locator)))
+                    if k_data == "text":
+                        textInteract(k, v)
+                    elif k_data == "datePicker":
+                        datePickerInteract(k, v)
+                    else:
+                        comboInteract(k, v)
                 except ECIexception:
                     print(f'Erro ao clicar no elemento {k}: ECIexception.')
                     return False
@@ -67,7 +87,6 @@ def registration_filling(filedir="./person.json"):
                     print(
                         f'Erro ao aguardar pelo elemento {k}: TimeoutException.')
                     return False
-                element.send_keys(v)
 
 
 wait.until(EC.visibility_of_element_located((
@@ -79,7 +98,7 @@ wait.until(EC.visibility_of_element_located((
 wait.until(EC.visibility_of_element_located((
     By.XPATH, "//button[contains(text(), 'Continuar')]"))).click()
 
-wait.until(EC.visibility_of_element_located((
+wait.until(EC.element_to_be_clickable((
     By.XPATH, "//und-menu-outlined-icon[@class = 'text-white menu-icon']"))).click()
 
 wait.until(EC.visibility_of_element_located((
